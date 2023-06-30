a5e2f163c5abee0d78ecf20c3074f12ad66570e7
Brandon Chung <chung.brandon123@gmail.com>
2023-06-29 21:02:47 -0500

    Update file structure

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import AreaChart from "@/components/charts/AreaChart";
-import { generateMonthLabel } from "@/utils/months";
-import { generateLineData } from "@/utils/generateData";
-import { generateColor } from "@/utils/generateColor";
-
-export default function Page() {
-	const data = generateLineData(12, 300, 2000);
-	return <div className="h-full">Dashboard</div>;
-}
