c7a90d41f5f5e1f4ceeafe1c555c921f87d23172
Brandon Chung <chung.brandon123@gmail.com>
2023-07-20 01:56:38 -0500

    Fix side drawer animation

--- src/components/nav/SideDrawer.tsx
+++ src/components/nav/SideDrawer.tsx
@@ -1,26 +1,18 @@
-"use client";
-
-import { Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import {
-	IconX,
 	IconChartAreaLine,
 	IconReportAnalytics,
+	IconX,
 } from "@tabler/icons-react";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
 import Path from "../path/Path";
 
 type SideDrawerProps = {
-	sideDrawerDisplayed: boolean;
-	closeSideDrawer(): void;
+	isOpen: boolean;
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export default function SideDrawer({
-	sideDrawerDisplayed,
-	closeSideDrawer,
-}: SideDrawerProps) {
-	const pathName = usePathname();
-
+export default function SideDrawer({ isOpen, setOpen }: SideDrawerProps) {
 	const paths = [
 		{
 			href: "/",
@@ -35,49 +27,72 @@ export default function SideDrawer({
 	];
 
 	return (
-		<>
-			<Transition show={sideDrawerDisplayed}>
+		<Transition.Root show={isOpen} as={Fragment}>
+			<Dialog as="div" className="relative z-50" onClose={setOpen}>
 				<Transition.Child
-					enter="transition-opacity ease-linear duration-100"
+					as={Fragment}
+					enter="ease-in-out duration-500"
 					enterFrom="opacity-0"
 					enterTo="opacity-100"
-					leave="transition-opacity ease-linear duration-100"
+					leave="ease-in-out duration-500"
 					leaveFrom="opacity-100"
 					leaveTo="opacity-0"
 				>
-					<div className="fixed inset-0 z-40">
-						<div className="flex relative z-10 flex-col w-80 h-full bg-gray-50 dark:bg-dark7 border-r border-gray-200 dark:border-dark8 xl:hidden">
-							<button
-								onClick={closeSideDrawer}
-								className="transition-all hover:ring-1 hover:ring-gray-700 dark:hover:ring-gray-400 flex absolute top-2 right-2 justify-center items-center w-9 h-9 rounded-full active:outline-none active:ring-2 active:ring-gray-600"
-								type="button"
-								value="Close sidebar"
-							>
-								<IconX className="w-5 h-5" />
-							</button>
-
-							<Path
-								className="flex flex-col p-6 overflow-y-auto flex-1 gap-2 mt-8"
-								paths={paths}
-							/>
-						</div>
-
-						<Transition.Child
-							enter="transition-opacity ease-linear duration-100"
-							enterFrom="opacity-0"
-							enterTo="opacity-100"
-							leave="transition-opacity ease-linear duration-100"
-							leaveFrom="opacity-100"
-							leaveTo="opacity-0"
-						>
-							<div
-								onClick={closeSideDrawer}
-								className="fixed inset-0 bg-black bg-opacity-80"
-							></div>
-						</Transition.Child>
-					</div>
+					<div className="fixed inset-0 bg-neutral-900 bg-opacity-80 transition-opacity" />
 				</Transition.Child>
-			</Transition>
-		</>
+
+				<div className="fixed inset-0 overflow-hidden">
+					<div className="absolute inset-0 overflow-hidden">
+						<div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full pr-10">
+							<Transition.Child
+								as={Fragment}
+								enter="transform transition ease-in-out duration-500 sm:duration-700"
+								enterFrom="-translate-x-full"
+								enterTo="translate-x-0"
+								leave="transform transition ease-in-out duration-500 sm:duration-700"
+								leaveFrom="translate-x-0"
+								leaveTo="-translate-x-full"
+							>
+								<Dialog.Panel className="pointer-events-auto relative w-screen max-w-xs sm:max-w-sm">
+									<Transition.Child
+										as={Fragment}
+										enter="ease-in-out duration-500"
+										enterFrom="opacity-0"
+										enterTo="opacity-100"
+										leave="ease-in-out duration-500"
+										leaveFrom="opacity-100"
+										leaveTo="opacity-0"
+									>
+										<div className="absolute right-0 top-0 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
+											<button
+												type="button"
+												className="text-zinc-950 dark:text-gray-300 transition-all hover:ring-1 hover:ring-gray-700 dark:hover:ring-gray-400 p-1 rounded-md active:outline-none active:ring-2 active:ring-gray-600"
+												onClick={() => setOpen(false)}
+											>
+												<span className="sr-only">Close panel</span>
+												<IconX className="h-6 w-6" aria-hidden="true" />
+											</button>
+										</div>
+									</Transition.Child>
+									<div className="flex h-full flex-col overflow-y-scroll bg-gray-50 dark:bg-dark7 py-6 shadow-xl">
+										<div className="px-4 sm:px-6">
+											<Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+												Panel title
+											</Dialog.Title>
+										</div>
+										<div className="relative mt-6 flex-1 px-4 sm:px-6">
+											<Path
+												className="flex flex-col overflow-y-auto flex-1 gap-2"
+												paths={paths}
+											/>
+										</div>
+									</div>
+								</Dialog.Panel>
+							</Transition.Child>
+						</div>
+					</div>
+				</div>
+			</Dialog>
+		</Transition.Root>
 	);
 }
